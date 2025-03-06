967fa2a8f12b046506dbb770e4ff33a548a6107d
JyothsnaJanjanam <janjanamjyothsna@gmail.com>
2025-03-06 10:27:16 +0530

    app completed

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,5 +1,5 @@
 import { Heart, ChefHat, WheatOff, Wheat } from "lucide-react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const RecipeCard = ({ recipe, bg, badge }) => {
   const [isFavorite, setIsFavorite] = useState(
